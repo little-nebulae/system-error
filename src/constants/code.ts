3119c80d2aa8992322cdb4bf2015f55e7c3d4492
Ryan Luong <lumintriet@gmail.com>
2026-09-16 18:32:19 +0700

    feat(code): add more codes.

--- src/constants/code.ts
+++ src/constants/code.ts
@@ -15,6 +15,26 @@ export const SYSTEM_ERROR_CODE_RECORD = {
     code: "ACCESS_DENIED_SYSTEM_ERROR",
     errno: ERRNO_RECORD.EACCES,
   },
+  ALREADY_EXISTED: {
+    code: "ALREADY_EXISTED_SYSTEM_ERROR",
+    errno: ERRNO_RECORD.EEXIST,
+  },
+  NOT_DIRECTORY: {
+    code: "NOT_DIRECTORY_SYSTEM_ERROR",
+    errno: ERRNO_RECORD.ENOTDIR,
+  },
+  IS_DIRECTORY: {
+    code: "IS_DIRECTORY_SYSTEM_ERROR",
+    errno: ERRNO_RECORD.EISDIR,
+  },
+  NAME_TOO_LONG: {
+    code: "NAME_TOO_LONG_SYSTEM_ERROR",
+    errno: ERRNO_RECORD.ENAMETOOLONG,
+  },
+  OVERFLOW: {
+    code: "OVERFLOW_SYSTEM_ERROR",
+    errno: ERRNO_RECORD.EOVERFLOW,
+  },
 } as const satisfies Record<
   string,
   { code: `${string}_SYSTEM_ERROR`; errno: ErrnoRecordValue }
