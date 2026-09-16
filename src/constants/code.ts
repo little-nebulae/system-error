7b403f7df8a1c10bc081e998835c37231befbe92
Ryan Luong <lumintriet@gmail.com>
2026-09-16 17:52:18 +0700

    feat(code): create SYSTEM_ERROR_CODE_RECORD constant.

---
+++ src/constants/code.ts
@@ -0,0 +1,21 @@
+import type { ErrnoRecord } from "@/constants/errno";
+
+import { ERRNO_RECORD } from "@/constants/errno";
+
+export const SYSTEM_ERROR_CODE_RECORD = {
+  PERMISSION_DENIED: {
+    code: "PERMISSION_DENIED_SYSTEM_ERROR",
+    errno: ERRNO_RECORD.EPERM,
+  },
+  NO_ENTRY: {
+    code: "NO_ENTRY_SYSTEM_ERROR",
+    errno: ERRNO_RECORD.ENOENT,
+  },
+  ACCESS_DENIED: {
+    code: "ACCESS_DENIED_SYSTEM_ERROR",
+    errno: ERRNO_RECORD.EACCES,
+  },
+} as const satisfies Record<
+  string,
+  { code: `${string}_SYSTEM_ERROR`; errno: ErrnoRecord[keyof ErrnoRecord] }
+>;
