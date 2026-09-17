f641bfc859dbe252dcf27572b5655defa8cade75
Ryan Luong <lumintriet@gmail.com>
2026-09-17 19:54:37 +0700

    feat: define lib entry.

--- src/classes/concrete/already-existed.ts
+++ src/classes/concrete/already-existed.ts
@@ -3,10 +3,10 @@ import type { BaseErrorMeta } from "@little-nebulae/error";
 import { SystemError } from "@/classes/abstract";
 import { SYSTEM_ERROR_RECORD } from "@/constants";
 
-export class AccessDeniedSystemError<
+export class AlreadyExistedSystemError<
   TMeta extends BaseErrorMeta = null,
-> extends SystemError<"ACCESS_DENIED_SYSTEM_ERROR", TMeta> {
-  readonly name = "AccessDeniedSystemError";
-  readonly code = SYSTEM_ERROR_RECORD.ACCESS_DENIED_SYSTEM_ERROR.code;
-  readonly errno = SYSTEM_ERROR_RECORD.ACCESS_DENIED_SYSTEM_ERROR.errno;
+> extends SystemError<"ALREADY_EXISTED_SYSTEM_ERROR", TMeta> {
+  readonly name = "AlreadyExistedSystemError";
+  readonly code = SYSTEM_ERROR_RECORD.ALREADY_EXISTED_SYSTEM_ERROR.code;
+  readonly errno = SYSTEM_ERROR_RECORD.ALREADY_EXISTED_SYSTEM_ERROR.errno;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+export * from "@/is-errno-exception";
+
+export * from "@/constants/errno";
+export * from "@/constants";
+
+export * from "@/classes/abstract";
+export * from "@/classes/concrete/access-denied";
+export * from "@/classes/concrete/already-existed";
+export * from "@/classes/concrete/file-too-big";
+export * from "@/classes/concrete/is-directory";
+export * from "@/classes/concrete/name-too-long";
+export * from "@/classes/concrete/no-entry";
+export * from "@/classes/concrete/not-directory";
+export * from "@/classes/concrete/out-of-memory";
+export * from "@/classes/concrete/overflow";
+export * from "@/classes/concrete/permission-denied";
+export * from "@/classes/concrete/process-interrupted";
+export * from "@/classes/concrete/resource-busy";
